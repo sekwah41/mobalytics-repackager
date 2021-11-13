33a7791a1d012d5ce763467f5f97b2fd12f0721a
Sekwah <contact@sekwah.com>
2021-11-13 02:44:54 +0000

    fix: Process now spawns

--- src/overlay-provider/provider-connector.js
+++ src/overlay-provider/provider-connector.js
@@ -1,17 +1,31 @@
-console.log("Some info");
-const electron_overlay = "../../electron_overlay";
-console.log(__dirname);
+const os = require("os");
 const path = require("path");
-const overlay = path.join(__dirname, electron_overlay);
-console.log(overlay);
-//const fs = require("fs");
-//console.log(fs.readFileSync(overlay + ".node", 'utf8'));
-const test = require(overlay);
-console.log(test);
+const fs = require("fs");
+
+console.log("Provider: Starting overlay")
+console.log("Provider: ProcessLocation", process.cwd());
+
+const electron_overlay = "electron_overlay";
+const electron_overlay_file = electron_overlay + ".node";
+const temp = os.tmpdir();
+const temp_provider = path.join(temp, electron_overlay_file);
+const overlay_loc = path.join(process.cwd(), electron_overlay_file);
+if(fs.existsSync(temp_provider)) {
+    if(fs.statSync(temp_provider).size !== fs.statSync(overlay_loc).size) {
+        console.log("Provider: Overlay file is different, copying");
+        fs.rmSync(temp_provider);
+        fs.copyFileSync(overlay_loc, temp_provider);
+    }
+} else {
+    fs.copyFileSync(overlay_loc, temp_provider);
+}
+const test = require(temp_provider);
 
 test.initMemoryScannerForLoL();
-console.log(test.getMemoryScannerForLoLStatus());
-console.log(test.getLoLPlayerLiveData());
+setInterval(() => {
+    console.log(test.getMemoryScannerForLoLStatus());
+    console.log(test.getLoLPlayerLiveData());
+}, 1000);
 
 // TODO write a connector which also returns what functions are available for construction on the other side
 // TODO create a connection though IPC or something which allows for direct calls (possibly with promise returns? i dunno)
