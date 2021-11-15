7ef28fcef6c899658bf37361302e54b1097b7db1
Sekwah <contact@sekwah.com>
2021-11-15 02:23:03 +0000

    feat: Creating server for MOBA-OVERLAY in wine

--- src/injection-connector/moba_server.js
+++ src/injection-connector/moba_server.js
@@ -2,14 +2,25 @@ const CLIENT_PATH = '\\\\.\\pipe\\MOBA-OVERLAY-CLIENT-';
 const SERVER_PATH = '\\\\.\\pipe\\MOBA-OVERLAY-SERVER';
 
 const net = require('net');
-const server = net.createServer((c) => {
+const server = net.createServer((socket) => {
     // 'connection' listener.
-    console.log('client connected');
-    c.on('end', () => {
-        console.log('client disconnected');
+    console.log("Overlay server connector", 'client connected to serer');
+    socket.on('end', () => {
+        console.log("Overlay server connector", 'client disconnected');
     });
-    c.write('hello\r\n');
-    c.pipe(c);
+    socket.on('data', (payload) => {
+        console.log("Overlay server connector", payload.toString(), this.processEvent);
+    });
+    socket.on('connect', () => {
+        console.log('Overlay server connector | socket connected');
+    });
+    socket.on('close', (hadError) => {
+        console.log(`Overlay server connector | socket closed, had_error: ${hadError}`);
+    });
+    socket.on('error', err => {
+        console.log('Overlay server connector | error: ', { err });
+    });
+
 });
 
 server.on('error', (err) => {
