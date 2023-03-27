1aaeba7a0cde9028c9a318732373da31a86b9eb7
Sekwah <contact@sekwah.com>
2023-03-27 14:11:28 +0100

    chore: new patch generation technique (still needs application script)

---
+++ src/recursive-diff.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const fs = require('fs');
+const { spawnSync } = require('child_process');
+const rimraf = require('rimraf');
+
+const currentDir = process.cwd();
+
+const patchesFolder = path.join(__dirname, '../patches');
+
+/**
+ * Recursively generate diff files for every file in a directory
+ */
+function recursiveDiff(root, folder) {
+    const files = fs.readdirSync(path.join(root, folder), { withFileTypes: true });
+    for (const file of files) {
+        if(file.isDirectory()) {
+            recursiveDiff(root, path.join(folder, file.name));
+        } else {
+            console.log("Checking file", path.join(folder, file.name));
+            const originalFile = path.relative(currentDir, path.join(root, "../original", folder, file.name));
+            const modifiedFile = path.relative(currentDir, path.join(root, folder, file.name));
+            const patchFolder = path.relative(currentDir, path.join(root, "../../patches", folder));
+            // There may be a better way to do this e.g. get the output and save the file in code
+            // Though
+            const command = `diff -uaN "${originalFile}" "${modifiedFile}"`; //  > "${patchFile}" || true
+            //console.log("Command to run", command);
+            const output = spawnSync("diff", ["-uaN", originalFile, modifiedFile]);
+            if(output.status === 1) {
+                // Make folder recursively if it doesn't exist
+                if(!fs.existsSync(patchFolder)) {
+                    fs.mkdirSync(patchFolder, { recursive: true });
+                }
+                const patchFile = path.relative(currentDir, path.join(patchFolder, file.name + ".diff"));
+                fs.writeFileSync(patchFile, output.stdout.toString());
+            }
+        }
+    }
+}
+
+console.log("Generating patches");
+rimraf.sync(patchesFolder);
+fs.mkdirSync(patchesFolder);
+recursiveDiff(path.join(__dirname, './modified/'), "");
---
+++ src/recursive-patch.js
@@ -0,0 +1,3 @@
+
+// TODO apply all of the files
+// TODO flag if a file is missing or doesnt need to be patched (do a log at the end of missing or partially failed ones)
